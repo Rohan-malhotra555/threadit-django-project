44e4197c43b271c4fe0b888aa36b12ed1b99531e
Rohan Malhotra <rohanmalhotra@Rohans-MacBook-Air.local>
2025-10-24 16:24:00 +0530

    Registered Community and Post models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Community, Post  # 1. Importing models
+# Registering models here
 
-# Register your models here.
+# 2. Telling the admin site to manage the Community model
+admin.site.register(Community)
+
+# 3. Telling the admin site to manage the Post model
+admin.site.register(Post)
+
